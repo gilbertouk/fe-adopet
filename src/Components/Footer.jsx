48f35391a7f875e1ea589a387fd65692b6beb328
Gilberto Silva <gilbertoantoniouk@gmail.com>
2023-11-02 18:55:57 +0000

    create home-page

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,21 @@
+function Footer() {
+  return (
+    <div>
+      <footer className="footer">
+        <p>
+          2023 - Desenvolvido por{' '}
+          <a
+            href="https://github.com/gilbertouk"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Gilberto da Silva
+          </a>
+          .
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
