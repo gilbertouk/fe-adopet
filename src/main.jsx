5d3723c52bb0ce64f6569bf252ad1c63eafa1ca6
Gilberto da Silva <gilbertoantoniouk@gmail.com>
2023-11-09 09:09:58 +0000

    add disable react devtools for production

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './context/AuthProvider.jsx';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import App from './App.jsx';
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
