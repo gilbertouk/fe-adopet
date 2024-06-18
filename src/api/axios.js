d57244c47aad1abefa88164358a8f387b07dc3e6
Gilberto da Silva <gilbertoantoniouk@gmail.com>
2024-06-18 18:28:20 +0100

    update BASE_URL to use render api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,16 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 // const BASE_URL = 'http://localhost:3000/api';
-const BASE_URL = 'https://api-adopet.gilbertosilva.dev/api';
+// const BASE_URL = 'https://api-adopet.gilbertosilva.dev/api';
+const BASE_URL = "https://adopet-txwv.onrender.com/api";
+
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+axios.defaults.headers.common["Content-Type"] = "application/json";
 
 export default axios.create({
   baseURL: BASE_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
-  headers: { 'Content-Type': 'application/json' },
+  headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
